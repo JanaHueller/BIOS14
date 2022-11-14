cce2357fd5090fea045b7deca4b9b8dd653bad57
JanaHueller <117172700+JanaHueller@users.noreply.github.com>
2022-11-14 15:40:57 +0100

    Update Exercise_butterflies.R

--- Exercise_butterflies.R
+++ Exercise_butterflies.R
@@ -25,14 +25,14 @@ plot(c(0.97, 1.03), means[1,], ylim=c(18, 40), xlim=c(0.8, 2.2),
      ylab="Developmental time (days)",
      xaxt="n", las=1, pch=c(21,16), col="white")
 axis(1, 1:2, labels=c("Barbarea", "Berteroa"))
-arrows(c(0.97,1.03), means[1,]-ses[1,], c(0.97,1.03), 
-       means[1,]+ses[1,], length=0.05, angle=90, code=3)
-arrows(c(1.97,2.03), means[2,]-ses[2,], c(1.97,2.03), 
-       means[2,]+ses[2,], length=0.05, angle=90, code=3)
-segments(0.97, means[1,1], 1.97, means[2,1], lty=2)
-segments(1.03, means[1,2], 2.03, means[2,2])
-points(c(0.97, 1.03), means[1,], pch=c(21,16), bg="white")
-points(c(1.97, 2.03), means[2,], pch=c(21, 16), bg="white")
+arrows(c(0.97,1.03), means[,1]-ses[,1], c(0.97,1.03), 
+       means[,1]+ses[,1], length=0.05, angle=90, code=3)
+arrows(c(1.97,2.03), means[,2]-ses[,2], c(1.97,2.03), 
+       means[,2]+ses[,2], length=0.05, angle=90, code=3)
+segments(0.97, means[1,1], 1.97, means[1,2], lty=2)
+segments(1.03, means[2,1], 2.03, means[2,2])
+points(c(0.97, 1.03), means[,1], pch=c(21,16), bg="white")
+points(c(1.97, 2.03), means[,2], pch=c(21, 16), bg="white")
 legend("topleft", c("Maternal host", "Barbarea", "Berteroa"), 
        bty="n", pch=c(NA,21,16))
 
